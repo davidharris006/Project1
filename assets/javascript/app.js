87ce3347eb9ce6442c805f0621847230fe46c805
Ali Sarkeshik <alisarkeshik@gmail.com>
2019-04-13 15:31:37 -0700

    Fixed spacing on inputs and added a photo of weather, dynamically adds thead with text

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,38 @@
 
 $(document).ready(function () {
-  
+
+
+
   function createAirlinedata(somearr) {
-    
+
+    // Creates the thead  
+    function firstRow() {
+      var newRow = $("<tr>").append(
+        $("<td>").text("Flight Price"),
+        $("<td>").text("Departure Date"),
+        $("<td>").text("Flight Class"),
+      );
+
+      $('#thead').append(newRow)
+    }
+
+    firstRow();
+
+    // Lists actual Flight Price, Departure Date, Flight Class
+
     for (let i = 0; i < 10; i++) {
-      var wordedclass ="";
-      var tripclass= somearr.data[i].trip_class;
+      var wordedclass = "";
+      var tripclass = somearr.data[i].trip_class;
+
       var tr = $('<tr>')
-      if (tripclass === 0 ){
+      if (tripclass === 0) {
         wordedclass = "Economy"
       }
       else if (tripclass === 1) {
         wordedclass = "Coach"
       }
-      tr.html('<td>' + somearr.data[i].value + '' + '</td>' + '<td>' + somearr.data[i].depart_date + '' + '</td>'+ '<td>' + wordedclass + '' + '</td>')
-      
-      
+
+      tr.html('<td>' + somearr.data[i].value + '' + '</td>' + '<td>' + somearr.data[i].depart_date + '' + '</td>' + '<td>' + wordedclass + '' + '</td>')
 
       $('#bodytable').append(tr)
 
@@ -33,14 +50,13 @@ $(document).ready(function () {
     // Takes the user inputs from the specified IDs
     from = $("#from").val().trim();
     destination = $("#destination").val().trim();
-    destinationcity = $("#destinationCity").val().trim();
     start = moment($("#startDate").val().trim())
-    end  = moment($("#endDate").val().trim());
+    end = moment($("#endDate").val().trim());
 
-    // $("#from").val("");
-    // $("#destination").val("");
-    // $("#startDate").val("");
-    // $("#endDate").val("");
+    $("#from").val("");
+    $("#destination").val("");
+    $("#startDate").val("");
+    $("#endDate").val("");
 
     $.ajaxPrefilter(function (options) {
       if (options.crossDomain && $.support.cors) {
@@ -49,33 +65,16 @@ $(document).ready(function () {
     });
 
     // url: `${countryURL}origin=SAN&destination=SFO&depart_date=${start.format("YYYY-MM")}&return_date=2019-09&token=0ec4333c4c239dc2eae21220f6504c30&currency=USD`,
-    
-   
-    var settings1 = {
-      "async": true,
-      "crossDomain": true,
-      "url": `https://api.yelp.com/v3/businesses/search?term=points%20of%20interest&location=${destinationcity}`,
-      "method": "GET",
-      "headers": {
-        "Authorization": "Bearer 8PjqRtWKJIqnBZiMXVyB_Vj0DSnztb_o9Nrn-vYpgAjiDiTmtoUn94UwnrLNfBYKa64OCp9zHcSsHaNfGOO2AaFqYuGjtmz2iJjgcNQ2Zo4UExt_foAbVBEfxAWwXHYx",
-        "cache-control": "no-cache",
-        "Postman-Token": "07addf4a-c766-4208-8aae-028e210c4bdb"
-      }
-    }
-    
-    $.ajax(settings1).done(function (response) {
-      console.log(response);
-    });
 
     $.ajax({
-      url: `http://api.travelpayouts.com/v2/prices/month-matrix?currency=usd&origin=${from}&destination=${destination}&month=${start.format("YYYY-MM-DD")}&show_to_affiliates=true&token=0ec4333c4c239dc2eae21220f6504c30`,
+      url: `http://api.travelpayouts.com/v2/prices/month-matrix?currency=usd&origin=LED&destination=HKT&month=${start.format("YYYY-MM-DD")}&show_to_affiliates=true&token=0ec4333c4c239dc2eae21220f6504c30&trip_duration=02`,
       method: "GET"
     }).then(function (response) {
       console.log(response)
       createAirlinedata(response)
     })
     $.ajax({
-      url: `http://api.travelpayouts.com/v2/prices/month-matrix?currency=usd&origin=${destination}&destination=${from}&month=${end.format("YYYY-MM-DD")}&show_to_affiliates=true&token=0ec4333c4c239dc2eae21220f6504c30`,
+      url: `http://api.travelpayouts.com/v2/prices/month-matrix?currency=usd&origin=LED&destination=HKT&month=${end.format("YYYY-MM-DD")}&show_to_affiliates=true&token=0ec4333c4c239dc2eae21220f6504c30&trip_duration=02`,
       method: "GET"
     }).then(function (response) {
       console.log(response)
@@ -85,7 +84,7 @@ $(document).ready(function () {
     var settings = {
       "async": true,
       "crossDomain": true,
-      "url": `https://api.yelp.com/v3/businesses/search?term=hotel&location=${destinationcity}`,
+      "url": `https://api.yelp.com/v3/businesses/search?term=hotel&location=${destination}`,
       "method": "GET",
       "headers": {
         "Authorization": "Bearer 8PjqRtWKJIqnBZiMXVyB_Vj0DSnztb_o9Nrn-vYpgAjiDiTmtoUn94UwnrLNfBYKa64OCp9zHcSsHaNfGOO2AaFqYuGjtmz2iJjgcNQ2Zo4UExt_foAbVBEfxAWwXHYx",
@@ -98,48 +97,48 @@ $(document).ready(function () {
       console.log(response);
       var result = response.businesses;
       console.log(result);
-      for (let i = 0; i < 9; i++)  {
+      for (let i = 0; i < 9; i++) {
         // Creating a div to hold the hotel
         var hotelDiv = $("<div class='hotel'>");
-  
+
         // Storing the name of the hotel
         var name = result[i].name;
         console.log(name);
-  
+
         // Storing the price of the hotel
         var price = result[i].price;
         console.log(price);
-  
+
         // Storing the URL of the hotel
         var url = result[i].url;
         console.log(url)
-  
+
         // Creating a p tag with info
         var p = $("<p>").text("Name: " + name);
-        var img = $('<img class="images" id ="image-'+ i + '">')
+        var img = $('<img class="images" id ="image-' + i + '">')
         img.attr("src", result[i].image_url)
-         
-        
+
+
         // Appending the p tag to the Hotel Div we created
-        hotelDiv.text(name  + 'Price Range: ' + price + 'Contact: Phone Number '+ result[i].phone)
+        hotelDiv.text(name + 'Price Range: ' + price + 'Contact: Phone Number ' + result[i].phone)
         hotelDiv.prepend(img)
-  
+
         // Append the hotelDiv to the "#yelp" div in the HTML
         $("#yelp").append(hotelDiv);
       };
     });
 
-    
+
 
   });
- 
 
 
-  
+
+
   function appendCheck() {
     var div = $("<div>");
     var checkbox = ["<img src=./images/hotel-gif.gif>", "<img src=./images/car.gif>"];
-    
+
     for (var i = 0; i < checkbox.length; i++) {
       div.append("&nbsp&nbsp&nbsp" + "<div id=submit-btn>" + "<input type='checkbox' checked='yes'>" + "</input>" + "&nbsp&nbsp" + checkbox[i] + "&nbsp&nbsp&nbsp");
       // div.append(checkbox[i]);
@@ -147,9 +146,9 @@ $(document).ready(function () {
     $(".check").append(div);
   }
   appendCheck();
-  
-  
-  
+
+
+
 
 });
 
