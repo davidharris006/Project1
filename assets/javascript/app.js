56e8c4ef5dd28eaf5bccb4b9b1f074b40df9845e
David Harris <davidharris006@gmail.com>
2019-04-13 18:13:20 -0700

    recorrections

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -64,17 +64,33 @@ $(document).ready(function () {
       }
     });
 
+    var settings1 = {
+      "async": true,
+      "crossDomain": true,
+      "url": `https://api.yelp.com/v3/businesses/search?term=points%20of%20interest&location=${destinationcity}`,
+      "method": "GET",
+      "headers": {
+        "Authorization": "Bearer 8PjqRtWKJIqnBZiMXVyB_Vj0DSnztb_o9Nrn-vYpgAjiDiTmtoUn94UwnrLNfBYKa64OCp9zHcSsHaNfGOO2AaFqYuGjtmz2iJjgcNQ2Zo4UExt_foAbVBEfxAWwXHYx",
+        "cache-control": "no-cache",
+        "Postman-Token": "07addf4a-c766-4208-8aae-028e210c4bdb"
+      }
+    }
+
+    $.ajax(settings1).done(function (response) {
+      console.log(response);
+    });
+
     // url: `${countryURL}origin=SAN&destination=SFO&depart_date=${start.format("YYYY-MM")}&return_date=2019-09&token=0ec4333c4c239dc2eae21220f6504c30&currency=USD`,
 
     $.ajax({
-      url: `http://api.travelpayouts.com/v2/prices/month-matrix?currency=usd&origin=LED&destination=HKT&month=${start.format("YYYY-MM-DD")}&show_to_affiliates=true&token=0ec4333c4c239dc2eae21220f6504c30&trip_duration=02`,
+      url: `http://api.travelpayouts.com/v2/prices/month-matrix?currency=usd&origin=${from}&destination=${destination}&month=${start.format("YYYY-MM-DD")}&show_to_affiliates=true&token=0ec4333c4c239dc2eae21220f6504c30` ,
       method: "GET"
     }).then(function (response) {
       console.log(response)
       createAirlinedata(response)
     })
     $.ajax({
-      url: `http://api.travelpayouts.com/v2/prices/month-matrix?currency=usd&origin=LED&destination=HKT&month=${end.format("YYYY-MM-DD")}&show_to_affiliates=true&token=0ec4333c4c239dc2eae21220f6504c30&trip_duration=02`,
+      url: `http://api.travelpayouts.com/v2/prices/month-matrix?currency=usd&origin=${from}&destination=${destination}&month=${start.format("YYYY-MM-DD")}&show_to_affiliates=true&token=0ec4333c4c239dc2eae21220f6504c30`,
       method: "GET"
     }).then(function (response) {
       console.log(response)
